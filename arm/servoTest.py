1dcbe1a64715ed4f1b9ee1453546423c4fe2645f
powersagitar <114868133+powersagitar@users.noreply.github.com>
2025-04-08 14:59:50 -0400

    update servo test

--- arm/servoTest.py
+++ arm/servoTest.py
@@ -1,9 +1,11 @@
 from adafruit_servokit import ServoKit
+
 # 1, 3, 5, 10 13
 kit = ServoKit(channels=16)
 
 while True:
-    #p=input('pin: ')
-    a=input('value: ')
-    for i in range(16):
-        kit.servo[i].angle = int(a)   
+    # p=input('pin: ')
+    pin = input("pin: ")
+    angle = input("value: ")
+
+    kit.servo[int(pin)].angle = int(angle)
