cab8c93d7e5e7d4ea24b2fd07909a84b92a4f2e4
PotatoNato <nxrayman19@gmail.com>
2025-04-08 12:20:22 -0400

    i2c prolly

--- servoTest.py
+++ servoTest.py
@@ -1,5 +1,6 @@
 from adafruit_servokit import ServoKit
-kit = ServoKit(channels=16)
+kit = ServoKit(channels=16, i2c=[1,3,5,10,13])
+kit.I2C()
 while True:
     a=input('enter:-')
     kit.servo[0].angle = int(a)   
